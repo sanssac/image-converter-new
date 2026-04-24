45d0b187f4a73c8aaea9d3f3c7e642081acbb638
sanchit sachdeva <sanchitsachdeva2005@gmail.com>
2026-04-24 20:12:16 +0530

    feat: implement new UI styling and update Vercel caching headers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'image_converter_cache_v4';
+const CACHE_NAME = 'image_converter_cache_v5';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -49,6 +49,6 @@ self.addEventListener('activate', (event) => {
           return caches.delete(key);
         }
       }));
-    })
+    }).then(() => self.clients.claim())
   );
 });
